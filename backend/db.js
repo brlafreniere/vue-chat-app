09cd57813f50d5026c2867fd632fbd841bd39e44
Blaine Lafreniere <blaine@lizardgizzards.com>
2019-04-23 16:21:54 +0000

    Add seperate DBs for dev/prod

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,24 @@
 module.exports = function () {
+    var environment = process.env.NODE_ENV;
+
+    if (!environment) {
+        environment = 'development';
+    }
+
+    console.log('environment: ' + environment);
+
+    var database = '';
+
+    if (environment == 'development') {
+        database = 'vue_chat_app_dev';
+    }
+
+    if (environment == 'production') {
+        database = 'vue_chat_app';
+    }
+
     var pgp = require('pg-promise')();
-    var db = pgp('postgres://vue_chat_app:vue_chat_app@localhost:5432/vue_chat_app');
+    const connStr = 'postgres://' + database + ':' + database + '@localhost:5432/' + database;
+    var db = pgp(connStr);
     return db;
 }
