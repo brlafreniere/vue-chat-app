dd768d1ed06b3ffebcac1216d8f8a099d4483602
Blaine Lafreniere <blaine@lizardgizzards.com>
2019-09-17 07:54:25 +0000

    Add development host to CORS allowed hosts

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-	origins 'vue-chat-app.lizardgizzards.com'
+    origins 'vue-chat-app.lizardgizzards.com', 'lab.lizardgizzards.com:4000'
 	resource '*',
 	  headers: :any,
 	  methods: %i(get post put patch delete options head)
