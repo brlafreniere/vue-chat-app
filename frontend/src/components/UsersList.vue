8f09c0b0ab5f78fd44a323055a61f78ec0a6b7c2
Blaine Lafreniere <blaine@lizardgizzards.com>
2019-10-10 05:47:01 +0000

    Get UsersList working

--- frontend/src/components/UsersList.vue
+++ frontend/src/components/UsersList.vue
@@ -2,22 +2,22 @@
 
 <template>
     <div id="users-list">
-        <div>
-            <span class="heading">ONLINE</span>
-        </div>
-        <ul>
-            <li v-for="user in online_users" :key="user.nickname">
-                {{ user.nickname }}
-            </li>
-        </ul>
-        <div>
-            <span class="heading">OFFLINE</span>
-        </div>
-        <ul>
-            <li v-for="user in offline_users" :key="user.nickname">
-                {{ user.nickname }}
-            </li>
-        </ul>
+        <section>
+            <header>ONLINE USERS</header>
+            <ul>
+                <li v-for="user in online_users" :key="user.nickname">
+                    {{ user.nickname }}
+                </li>
+            </ul>
+        </section>
+        <section>
+            <header>OFFLINE USERS</header>
+            <ul>
+                <li v-for="user in offline_users" :key="user.nickname">
+                    {{ user.nickname }}
+                </li>
+            </ul>
+        </section>
     </div>
 </template>
 
@@ -32,21 +32,29 @@ export default {
         }
     },
     mounted () {
-        this.$nextTick( async () => {
+        this.$nextTick( () => {
+            this.loadUsers()
+        })
+        window.setInterval( () => {
+            this.loadUsers()
+        }, 30000) // every 30 seconds
+    },
+    methods: {
+        async loadUsers() {
             let url = `${process.env.VUE_APP_API_URL}/chat_room/${this.currentRoom.id}/users`
             let response = await this.axios.get(url)
             this.users = response.data
-        })
+        }
     },
     computed: {
         online_users: function () {
             return this.users.filter((user) => {
-                return user.online_status
+                return user.online
             })
         },
         offline_users: function () {
             return this.users.filter((user) => {
-                return !user.online_status
+                return !user.online
             })
         }
     }
@@ -61,8 +69,18 @@ export default {
     margin-left: 1em;
 }
 
-#users-list > div {
+#users-list {
     padding: 1em;
+}
+
+section {
+    margin-bottom: 50px;
+}
+
+header {
+    background-color: #5f367a;
+    padding: 5px;
     text-align: center;
+    border-radius: 5px;
 }
 </style>
