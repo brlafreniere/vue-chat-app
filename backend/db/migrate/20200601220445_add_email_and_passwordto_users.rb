a97380f515379992acec0de3577e7c6a98e9d61e
Ubuntu <ubuntu@demos.lizardgizzards.com>
2020-06-21 20:38:18 +0000

    Fix typo

--- backend/db/migrate/20200601220445_add_email_and_passwordto_users.rb
+++ backend/db/migrate/20200601220445_add_email_and_passwordto_users.rb
@@ -1,6 +1,6 @@
 class AddEmailAndPasswordtoUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :email, :string
-    add_column :users, :password_digest, :strina
+    add_column :users, :password_digest, :string
   end
 end
