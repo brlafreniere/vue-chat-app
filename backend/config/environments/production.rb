2413ffb48d01c7b8489aeeb106b91a2c6a5f8319
Blaine Lafreniere <blaine@lizardgizzards.com>
2020-06-21 02:05:10 -0600

    Update production origin for SSL

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = '/cable'
-  config.action_cable.allowed_request_origins = [ 'http://vuechat.blainelafreniere.io' ]
+  config.action_cable.allowed_request_origins = [ 'https://vuechat.blainelafreniere.io' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
